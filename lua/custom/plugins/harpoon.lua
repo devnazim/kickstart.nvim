42bc354840c2af266847b7d5b47293adbff84c3d
Nazim <dev.nazim.k@gmail.com>
2024-02-29 12:22:17 +0500

    fix harpoon file navigation shortcuts

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,7 +6,7 @@ vim.keymap.set('n', '<C-t>', ui.toggle_quick_menu, { desc = 'Add file to harpoon
 
 vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<C-j>', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<C-n>', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<C-l>', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<C-l>', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<C-n>', function() ui.nav_file(4) end)
 
 return {}
